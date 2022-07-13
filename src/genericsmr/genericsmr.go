b44b8e42ee994b4874846dd0ef6306f31e15ae3d
pasindu <pasindu.13@cse.mr.ac.lk>
2022-07-13 15:33:56 +0200

    solved epaoxs binding issue

--- src/genericsmr/genericsmr.go
+++ src/genericsmr/genericsmr.go
@@ -192,7 +192,7 @@ func (r *Replica) waitForPeerConnections(done chan bool) {
 	var b [4]byte
 	bs := b[:4]
 
-	r.Listener, _ = net.Listen("tcp", "localhost:"+getPort(r.PeerAddrList[r.Id]))
+	r.Listener, _ = net.Listen("tcp", "0.0.0.0:"+getPort(r.PeerAddrList[r.Id]))
 	for i := r.Id + 1; i < int32(r.N); i++ {
 		conn, err := r.Listener.Accept()
 		if err != nil {
