e3d268f5fe112b1ab3278f65351cb7de1e650339
Pasindu Tennage <pasindu.13@cse.mrt.ac.lk>
2023-02-18 17:07:27 +0100

    added local-automation

--- experiments/local_automation.py
+++ experiments/local_automation.py
@@ -1,23 +1,66 @@
 import os
 
-for arrival in [1000, 20000]:
-    for batchSize in [1, 50, 3000]:
-        for batchTime in [100, 2000]:
-            for leaderTimeout in [1000, 3000, 1000000]:
-                for pipeline in [1, 10, 100]:
-                    os.system("/bin/bash /home/pasindu/Documents/epaxos/experiments/local-consensus-epaxos-test.sh "+ str(arrival) + " "
+def run(arrival, batchSize, batchTime, leaderTimeout, pipeline):
+    os.system("/bin/bash /home/pasindu/Documents/epaxos/experiments/local-consensus-epaxos-test.sh "+ str(arrival) + " "
                               + "-pa" + " "
                               + str(batchSize) + " "
                               + str(batchTime) + " "
                               + str(leaderTimeout) + " "
                               + str(pipeline) + " "
                               + "-l" + " ")
-
-                    os.system("/bin/bash /home/pasindu/Documents/epaxos/experiments/local-consensus-epaxos-test.sh "+ str(arrival) + " "
+    os.system("/bin/bash /home/pasindu/Documents/epaxos/experiments/local-consensus-epaxos-test.sh "+ str(arrival) + " "
                               + "-e" + " "
                               + str(batchSize) + " "
                               + str(batchTime) + " "
                               + str(leaderTimeout) + " "
                               + str(pipeline))
+# case 1
+arrival=1000
+batchSize=1
+batchTime=1
+leaderTimeout=1000000
+pipeline=1
+run(arrival, batchSize, batchTime, leaderTimeout, pipeline)
 
 
+# case 2
+arrival=10000
+batchSize=50
+batchTime=50
+leaderTimeout=1000000
+pipeline=1
+run(arrival, batchSize, batchTime, leaderTimeout, pipeline)
+
+
+# case 3
+arrival=10000
+batchSize=50
+batchTime=50
+leaderTimeout=1000000
+pipeline=10
+run(arrival, batchSize, batchTime, leaderTimeout, pipeline)
+
+
+# case 4
+arrival=10000
+batchSize=50
+batchTime=50
+leaderTimeout=10000
+pipeline=1
+run(arrival, batchSize, batchTime, leaderTimeout, pipeline)
+
+# case 5
+arrival=10000
+batchSize=50
+batchTime=50
+leaderTimeout=1000
+pipeline=1
+run(arrival, batchSize, batchTime, leaderTimeout, pipeline)
+
+# case 6
+arrival=10000
+batchSize=50
+batchTime=50
+leaderTimeout=100
+pipeline=1
+run(arrival, batchSize, batchTime, leaderTimeout, pipeline)
