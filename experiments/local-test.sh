48b6d94e9142ef2616acb5b393d6b4e66e2f3173
Pasindu Tennage <pasindu.13@cse.mrt.ac.lk>
2023-05-23 16:00:49 +0200

    checked local-test.sg

--- experiments/local-test.sh
+++ experiments/local-test.sh
@@ -1,4 +1,3 @@
-# A test that tests epaxos by sending client requests
 arrival=$1
 algo=$2
 batchSize=$3
@@ -12,8 +11,6 @@ server_path="bin/epaxos_server"
 ctl_path="bin/epaxos_client"
 master_path="bin/epaxos_master"
 output_path="logs/"
-
-# rm -r bin/; mkdir bin ; GOPATH=/home/pasindu/Documents/epaxos; GO111MODULE=off; cd src/master ; go build -o master .; mv master /home/pasindu/Documents/epaxos/bin/epaxos_master; cd ../.. ; cd src/server ; go build -o server .; mv server /home/pasindu/Documents/epaxos/bin/epaxos_server; cd ../.. ; cd src/client ; go build -o client .; mv client /home/pasindu/Documents/epaxos/bin/epaxos_client; cd ../.. ; GO111MODULE=on
 
 rm -r ${output_path}; mkdir ${output_path}
 
